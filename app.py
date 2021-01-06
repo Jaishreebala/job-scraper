ea11ba035222e980dea3de4efb928f088e1917af
Jaishree Bala <jaishree1bala@gmail.com>
2021-01-05 23:00:20 -0500

    Search By User Input

--- app.py
+++ app.py
@@ -2,23 +2,40 @@ from bs4 import BeautifulSoup
 import requests
 from lxml import html
 
-field = "web"
-location = "Waterloo%2C%2BOntario%2C%2BCanada"
+field = input("Enter Field: ")
+location = input("Enter Location [eg: Waterloo, Ontario, Canada]: ")
+location = location.replace(",", "%2C")
+skills = []
+
+while True:
+    skillInput = input("Enter Skill (type done when done): ")
+    if(skillInput == "done"):
+        break
+    else:
+        skills.append(skillInput)
+
+print(skills)
+
 pageNo = 0
-reqstring = f'https://www.linkedin.com/jobs/search?keywords={field}&location={location}&trk=homepage-jobseeker_jobs-search-bar_search-submit&currentJobId=2342605609&position=2&pageNum={pageNo}'
+reqstring = f'https://www.linkedin.com/jobs/search?keywords={field}&location={location}&trk=homepage-jobseeker_jobs-search-bar_search-submit&position=2&pageNum={pageNo}'
 html_text = requests.get(reqstring).text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all(
     'li', class_="result-card job-result-card result-card--with-hover-state")
 
-for job in jobs:
-    link = job.find('a', class_="result-card__full-card-link")
-    jobTitle = job.find(
-        'h3', class_="result-card__title job-result-card__title").text
-    employer = job.find(
-        'a', class_="result-card__subtitle-link job-result-card__subtitle-link").text
-    print(f"""
-    Link: {link.get('href')}\n
-    Job Tile: {jobTitle}\n 
-    Employer: {employer}
-    """)
+if len(jobs) == 0:
+    print("no results found")
+else:
+    for job in jobs:
+        link = job.find('a', class_="result-card__full-card-link")
+        jobTitle = job.find(
+            'h3', class_="result-card__title job-result-card__title").text
+        employer = job.find(
+            'a', class_="result-card__subtitle-link job-result-card__subtitle-link")
+        if (employer):
+            employer = employer.text
+        print(f"""
+        Link: {link.get('href')}\n
+        Job Tile: {jobTitle}\n
+        Employer: {employer}
+        """)
--- jobs.py
+++ jobs.py
@@ -3,20 +3,19 @@ import requests
 import lxml
 
 import urllib
+import json
 import httplib2
 
 # url = 'https://adfs.uwaterloo.ca/adfs/ls/?client-request-id=ee2d7ff9-a639-4ffa-f302-00800210003b'
-url = 'https://waterlooworks.uwaterloo.ca/myAccount/dashboard.htm'
-headers = {'Content-type': 'application/x-www-form-urlencoded', 'Cookie': {'BIGipServerCECA_443.app~CECA_443_pool':
-                                                                           'rd2o00000000000000000000ffffac108932o23111', 'JSESSIONID': '3FF5EC3E229CC54EF8819DB52F2A17B7'}}
-# headers['BIGipServerCECA_443.app~CECA_443_pool'] = 'rd2o00000000000000000000ffffac108932o23111'
-# headers['JSESSIONID'] = '3FF5EC3E229CC54EF8819DB52F2A17B7'
-print(headers)
+url = 'https://adfs.uwaterloo.ca/adfs/ls/'
+body = {'UserName': 'jbalamur@uwaterloo.ca', 'Password': 'nuhnesW-1102'}
+headers = {'Content-type': 'application/x-www-form-urlencoded'}
+
 http = httplib2.Http()
 response, content = http.request(
-    url, 'GET', headers=headers)
+    url, 'POST', headers=headers, body=urllib.parse.urlencode(body))
 
-# print(response)
-# soup = BeautifulSoup(html_text, 'lxml')
+print(response)
+# print(conten)
 
-# print(soup.prettify())
+# headers = {'Cookie': response['set-cookie']}
