ff403b7e998364151d1123ba6ced3e3790022e86
Jaishree Balamurugan <52601237+Jaishreebala@users.noreply.github.com>
2021-01-06 05:26:52 -0500

    Delete glassdoor.py

--- glassdoor.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-from lxml import html
-
-reqstring = 'https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=web+developer&locT=&locId=&jobType=&context=Jobs&sc.keyword=web+developer&dropdown=0'
-html_text = requests.get(reqstring).text
-soup = BeautifulSoup(html_text, 'lxml')
-print(soup)
